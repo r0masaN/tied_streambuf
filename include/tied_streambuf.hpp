7fde86c345f86e224724abf734618dcb0c840771
Roman <118828810+r0masaN@users.noreply.github.com>
2026-03-27 01:50:27 +0300

    created tied_streambuf.hpp

---
+++ include/tied_streambuf.hpp
@@ -0,0 +1,32 @@
+#ifndef TIED_STREAMBUF_HPP
+#define TIED_STREAMBUF_HPP
+
+#include <streambuf>
+
+template<typename CharT, std::size_t N>
+class tied_streambuf : public std::basic_streambuf<CharT> {
+    using traits_type = std::char_traits<CharT>;
+    using char_type = traits_type::char_type;
+    using int_type = traits_type::int_type;
+
+    std::basic_streambuf<char_type> *buffers_[N];
+
+public:
+    template<typename... Streambuf>
+        requires (... && std::is_convertible_v<Streambuf *, std::basic_streambuf<char_type> *>)
+    explicit tied_streambuf(Streambuf *... buffers) noexcept;
+
+private:
+    std::streamsize xsputn(const char_type *s, std::streamsize n) final;
+
+    int_type overflow(int_type c) final;
+
+    int sync() final;
+};
+
+template<typename First, typename... Other>
+tied_streambuf(First *, Other *...) -> tied_streambuf<typename First::char_type, 1 + sizeof...(Other)>;
+
+#include "tied_streambuf.tcc"
+
+#endif
